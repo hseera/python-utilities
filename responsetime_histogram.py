5b610b0a49a06cc385ec75ff67e6485a9916d521
hseera <harinderseera@gmail.com>
2021-02-14 11:07:19 +1100

    histogram update
    updated histogram image and script

--- responsetime_histogram.py
+++ responsetime_histogram.py
@@ -13,7 +13,7 @@ df = pd.read_csv(FILE)
 x = []
 x = df['elapsed_time'].dropna() #replace "elapsed_time' with the correct colume name. Add dropna() incase you have empty row values
 
-plt.figure(figsize=(12,12))
+plt.figure(figsize=(10,6))
 
 #parameters for the histogram. Update the values when you want to plot multiple histogram together
 #kwargs = dict(histtype='step', stacked=False, alpha=0.7, fill=False, bins=500)
@@ -21,7 +21,7 @@ kwargs = dict(histtype='stepfilled', stacked=False, alpha=0.7, fill=True, bins=5
 
 #define historgram label
 plt.xlabel('Response Time (ms)')
-plt.ylabel('Total Count/Bin')
+plt.ylabel('Frequency')
 
 #plot histogram
 plt.hist(x, **kwargs)
