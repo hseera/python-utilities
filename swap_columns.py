b831d984dc985d1d666a456ac348164ca52a4214
hseera <harinderseera@gmail.com>
2021-02-15 14:11:31 +1100

    Update swap_columns.py

--- swap_columns.py
+++ swap_columns.py
@@ -6,10 +6,10 @@ import pandas as pd
 Below is an example of rearranging jmeter jtl/csv file.
 
 '''
-# rearrange column here
-columns =['timeStamp','label','elapsed','Latency','IdleTime','Connect','responseCode',
-          'responseMessage','dataType','success','failureMessage',
-          'bytes','sentBytes','grpThreads','allThreads','URL','threadName']
+# rearranged column here
+columns =['timeStamp','label','elapsed','Latency','Connect','IdleTime','responseCode',
+          'responseMessage','bytes','dataType','success',
+          'failureMessage','sentBytes','URL','threadName','allThreads','grpThreads']
 
 df = pd.read_csv('./sample_files/swap_001.csv')
 df_reordered = df[columns] 
