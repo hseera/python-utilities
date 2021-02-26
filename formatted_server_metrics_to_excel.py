0bf8911b98953b913d3fe1fd078b7dee1edbac2c
hseera <harinderseera@gmail.com>
2021-02-26 11:42:38 +1100

    Update formatted_server_metrics_to_excel.py

--- formatted_server_metrics_to_excel.py
+++ formatted_server_metrics_to_excel.py
@@ -15,32 +15,40 @@ Therefore this script will require minor modification to cater for your scenario
 #import openpyxl
 import pandas as pd
 
-def save_data_to_xls(file_to_read):
-    try:        
-        df = pd.read_excel(file_to_read)
-        
+def format_data(file_to_read):
+    try:
         '''
         For each unique name in the column "TARGET_NAME", extract all the data for it and generate a pivot table.
-        Once pivot table is generated, save that data into the same xlsx file as a new sheet. The sheet name is the TARGET_NAME.
+        Once pivot table is generated, save that data into the same xlsx file as a new sheet. 
+        The sheet name is the TARGET_NAME.
         '''
+        df = pd.read_excel(file_to_read)
         for target in df.TARGET_NAME.unique(): 
-            x = df.loc[df['TARGET_NAME'] == target] #filter out all the rows for which the label column does not contain value GetDistribution
+            x = df.loc[df['TARGET_NAME'] == target] #filter out all the rows for which the label column does not contain value target 
             
             res = x.pivot_table(index=['TimeStamp','METRIC_COLUMN'], values='VALUE', aggfunc='sum').reset_index()
             pivot = res.pivot(index='TimeStamp', columns='METRIC_COLUMN', values='VALUE')
+            
             pivot = pivot.fillna(0) # fill all cells with NaN with value 0. Change it as you see fit for your usecase.
             
-            writer = pd.ExcelWriter(file_to_read,  engine='openpyxl', mode='a') #engine='xlsxwriter'
-            pivot.to_excel(writer, index=True, sheet_name=target)
-            writer.bookworksheet = writer.sheets[target]
-            writer.save()
-            writer.close()
+            save_data_to_xls(file_to_read, target, pivot)
+            
+    except Exception as e:
+        print(e)
+
+def save_data_to_xls(file_to_read, sheet_name, pivoted_data):
+    try:                
+        writer = pd.ExcelWriter(file_to_read, engine='openpyxl', mode='a') #engine='xlsxwriter' <-- doesn't support append mode
+        pivoted_data.to_excel(writer, index=True, sheet_name=sheet_name)
+        writer.bookworksheet = writer.sheets[sheet_name]
+        writer.save()
+        writer.close()
     except Exception as e:
         print(e)
    
 def main():
     FILE_TO_READ = './sample_files/log-file-database.xlsx' #sample data file
-    save_data_to_xls(FILE_TO_READ)
+    format_data(FILE_TO_READ)
 
 if __name__ == "__main__":
     main()
