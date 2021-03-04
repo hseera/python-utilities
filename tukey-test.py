25dfb6599e8a85d13af43a13bd80cc11d1967a7f
hseera <harinderseera@gmail.com>
2021-03-04 18:41:40 +1100

    updated the tukey script

--- tukey-test.py
+++ tukey-test.py
@@ -21,21 +21,25 @@ def tukey_test(file, variable_name):
         
         first_quartile = df[variable_name].quantile(0.25) #q1
         third_quartile = df[variable_name].quantile(0.75) #q3
-        
+         
         interquartile_range = third_quartile-first_quartile #irq = g3 - q1
         
         lower_inner_fence = first_quartile - k1*interquartile_range 
         upper_inner_fence = third_quartile + k1*interquartile_range
         
+        print("Outlier Range (k=%s): [%s, %s]\n" %(k1, lower_inner_fence,upper_inner_fence))
+        
         lower_outer_fence = first_quartile - k2*interquartile_range
         upper_outer_fence = third_quartile + k2*interquartile_range
-               
+        
+        print("Far Outlier Range (k=%s): [%s, %s]\n" %(k2,lower_outer_fence,upper_outer_fence))
+        
         for index, x in enumerate(df[variable_name]):
             if x >= upper_inner_fence or x <= lower_inner_fence:
                 outliers.append(x)
             if x >= upper_outer_fence or x <= lower_outer_fence: 
                 far_outliers.append(x)
-        return k1, k2, outliers, far_outliers 
+        return k1, k2, sorted(outliers), sorted(far_outliers)
     except Exception as e:
         print(e)
     
@@ -43,8 +47,8 @@ def tukey_test(file, variable_name):
 def main():
     FILE = "./sample_files/tukey_test.csv" #replace with your file name
     k1, k2, outliers, far_outliers = tukey_test(FILE, "results_001")
-    print("Potential outliers (k=%s): %s" %(k1, outliers))
-    print("\nPotential far out outliers (k=%s): %s" %(k2,far_outliers))
+    print("Potential outliers not in range (k=%s): %s\n" %(k1, outliers))
+    print("Potential far out outliers not in range (k=%s): %s\n" %(k2,far_outliers))
     
 if __name__ == "__main__":
     main()
