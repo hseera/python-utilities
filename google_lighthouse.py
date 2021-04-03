655962df6c90b3e1c49366c105b64118bfcbe321
hseera <harinderseera@gmail.com>
2021-04-03 13:27:13 +1100

    Update google_lighthouse.py

--- google_lighthouse.py
+++ google_lighthouse.py
@@ -4,13 +4,24 @@ import requests
 import json
 from datetime import datetime
 
+#Query pagespeed api with requested url 
+def execute_pagespeed(pagespeed_base_url, params):
+    response = requests.get(pagespeed_base_url, params = params) #, timeout=5.5
+    if (response.status_code == 200):
+        extract_metrics(response.text)
+    else:
+        print("Invalid Response OR Invalid Key")
+
 #extract lighthouse metrics
-def extract_metrics(pagespeed_response):
+def extract_metrics(api_response):
     try:
         metrics = {}  
-        fullresults = json.loads(pagespeed_response)
+        
+        fullresults = json.loads(api_response)
+        
         measuretime = datetime.now()
         metrics['measurement_date'] = measuretime.strftime('%Y-%m-%d %H:%M:%S')
+        
         if (fullresults.get('lighthouseResult') != None):
             lighthouse_result = fullresults.get('lighthouseResult')
             metrics['requestedUrl'] = lighthouse_result['requestedUrl']
@@ -33,30 +44,32 @@ def extract_metrics(pagespeed_response):
                 except Exception:
                     metrics[category] = 'null'
             
-            metrics['largest-contentful-paint'] = lighthouse_result['audits']['largest-contentful-paint']['numericValue']
-            metrics['first-contentful-paint'] = lighthouse_result['audits']['first-contentful-paint']['numericValue']
-            metrics['interactive'] = lighthouse_result['audits']['interactive']['numericValue']
-            metrics['speed-index'] = lighthouse_result['audits']['speed-index']['numericValue']
-            metrics['total-blocking-time'] = lighthouse_result['audits']['total-blocking-time']['numericValue']
-            metrics['cumulative-layout-shift'] = lighthouse_result['audits']['cumulative-layout-shift']['numericValue']
-            metrics['numRequests'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['numRequests']
-            metrics['numScripts'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['numScripts']
-            metrics['maxServerLatency'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['maxServerLatency']
-            metrics['throughput'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['throughput']
-            metrics['numTasksOver100ms'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['numTasksOver100ms']
-            metrics['numFonts'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['numFonts']
-            metrics['numTasksOver500ms'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['numTasksOver500ms']
-            metrics['numStylesheets'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['numStylesheets']
-            metrics['numTasksOver10ms'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['numTasksOver10ms']
-            metrics['numTasksOver50ms'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['numTasksOver50ms']
-            metrics['mainDocumentTransferSize'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['mainDocumentTransferSize']
-            metrics['numTasksOver25ms'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['numTasksOver25ms']
-            metrics['totalTaskTime'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['totalTaskTime']
-            metrics['maxRtt'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['maxRtt']
-            metrics['numTasks'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['numTasks']
-            metrics['rtt'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['rtt']
-            metrics['totalByteWeight'] = lighthouse_result['audits']['diagnostics']['details']['items'][0]['totalByteWeight']
+            #Get performance metric data
+            perf_metrics = [ 'largest-contentful-paint','first-contentful-paint','interactive',
+					  'speed-index','total-blocking-time','cumulative-layout-shift' ]
+            
+            for perf_metric in  perf_metrics:
+                try:
+                    metrics[perf_metric] = lighthouse_result['audits'][perf_metric]['numericValue']
+                except Exception:
+                    metrics[perf_metric] = 'null'
+            
+            #Get debug metric data
+            debug_metrics = [ 'numRequests','numScripts','maxServerLatency',
+					  'throughput','numTasksOver100ms','numFonts',
+                      'numTasksOver500ms','numStylesheets','numTasksOver10ms', 
+                      'numTasksOver50ms','mainDocumentTransferSize','numTasksOver25ms',
+                      'totalTaskTime','maxRtt','numTasks',
+                      'rtt','totalByteWeight']
+            
+            for debug_metric in  debug_metrics:
+                try:
+                    metrics[debug_metric] = lighthouse_result['audits']['diagnostics']['details']['items'][0][debug_metric]
+                except Exception:
+                    metrics[debug_metric] = 'null'
+            
         display_metrics(metrics)
+    
     except Exception as ex:
         print(ex)
 
@@ -64,19 +77,11 @@ def extract_metrics(pagespeed_response):
 #Shown captured metrics for the url. Replace/Update this function to save the data to a timeseries database.
 def display_metrics(metrics):
      print(metrics)
-
-#Invoke pagespeed api 
-def execute_pagespeed(pagespeed_url, params ):
-    response = requests.get(pagespeed_url, params = params) #, timeout=5.5
-    if (response.status_code==200):
-        extract_metrics(response.text)
-    else:
-        print("Invalid Response OR Invalid Key")
   
 def main():
     #api parameters
     requested_url='https://www.bbc.co.uk/'   #replace the requested url with your url
-    apikey='AIzaSyA5ILlS...'  # replace apikey with your pagespeed api key
+    apikey='AIzaSyA5ILlSdcBWs9DasxLGIpxiYinzGjWV84A'  # replace apikey with your pagespeed api key
     pagespeed_service_url = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed'
     parameters = { 'url': requested_url,
         'key': apikey ,
